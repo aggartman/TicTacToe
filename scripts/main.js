24c9ce26ded97def2fb597e8f33e83967bbd6ae8
Austin Gartman <aggartman@gmail.com>
2021-09-11 15:13:28 -0500

    Finished with Javascript functionality

--- scripts/main.js
+++ scripts/main.js
@@ -14,7 +14,7 @@ const gameBoard = (() => {
         const gameMode2 = document.getElementById('aiPlayer');
         const players = document.getElementById('players');
         const playerChoice = document.getElementById('playerChoice')
-        const gameTxt = document.getElementById('gameTxt');
+        const gameTxt = document.getElementById('gameTxt');                const tiles = document.getElementsByClassName('tile');
         gameTxt.style.display = 'block'
         switch (mode) {
             case player2:
@@ -22,12 +22,20 @@ const gameBoard = (() => {
                 playerChoice.style.display = 'block'
                 gameMode1.style.display = 'block';
                 gameMode2.style.display = 'none';
+                for (i = 0; i < tiles.length; i++) {
+                    tiles[i].addEventListener('click', placeToken);
+                    tiles[i].removeEventListener('click', aiGame);
+                }
                 break;
             case ai: 
                 players.style.display = 'none';
-                playerChoice.style.display = 'block'
-                gameMode1.style.display = 'none'
-                gameMode2.style.display = 'block'
+                playerChoice.style.display = 'block';
+                gameMode1.style.display = 'none';
+                gameMode2.style.display = 'block';
+                for (i = 0; i < tiles.length; i++) {
+                    tiles[i].addEventListener('click', aiGame);
+                    tiles[i].removeEventListener('click', placeToken);
+                }
                 break;
             default: 
                 break;
@@ -43,12 +51,35 @@ const gameBoard = (() => {
         }
         return [token1, token2]
     }
-
+    const computerToken = () => {
+        var markers = document.getElementsByClassName('marker');
+        const computerToken = document.getElementById('token2').innerText;
+        var emptyTiles = [];
+        for (let i = 0; i < markers.length; i++) {
+            if (markers[i].innerText == '') {
+                emptyTiles.push(markers[i].id);
+            }
+        } 
+        var computerTile = emptyTiles[Math.floor(Math.random() * emptyTiles.length)];
+        markers[computerTile].innerText = computerToken;
+        console.log(markers[computerTile])
+    }
     const tileCheck = (marker1, marker2, marker3) => {
         const gameOver = document.getElementById('gameOver');
+        let gameOverTxt = document.getElementById('gameOverTxt');
         if (marker1 != '' || marker2 != '' || marker3 != '') {
             if ((marker1 === 'X' && marker2 === 'X' && marker3 === 'X') || (marker1 === 'O' && marker2 === 'O' && marker3 === 'O')) {
+                let player = '';
+                let token = document.getElementById('token1').innerText;
+                if (marker1 === token) {
+                    player = document.getElementById('player1').innerText;
+        
+                } else {
+                    player = document.getElementById('player2').innerText;
+                }
+                player = player.slice(0, -1);
                 gameOver.style.display = 'block';
+                gameOverTxt.innerText = `${player} wins!`;
                 gameOver.addEventListener('click', function() {
                     clearBoard();
                     gameOver.style.display = 'none';
@@ -67,6 +98,8 @@ const gameBoard = (() => {
         var tile7 = document.getElementById('markSeven').innerText;
         var tile8 = document.getElementById('markEight').innerText;
         var tile9 = document.getElementById('markNine').innerText;
+        const gameOver = document.getElementById('gameOver');
+        let gameOverTxt = document.getElementById('gameOverTxt');
         tileCheck(tile1, tile2, tile3);
         tileCheck(tile4, tile5, tile6);
         tileCheck(tile7, tile8, tile9);
@@ -75,6 +108,14 @@ const gameBoard = (() => {
         tileCheck(tile2, tile5, tile8);
         tileCheck(tile3, tile6, tile9);
         tileCheck(tile7, tile5, tile3);
+        if (tile1 != '' && tile2 != '' && tile3 != '' && tile4 != '' && tile5 != '' && tile6 != '' && tile7 != '' && tile8 != '' && tile9 != '') {
+            gameOver.style.display = 'block';
+            gameOverTxt.innerText = 'Game Over'
+                gameOver.addEventListener('click', function() {
+                    clearBoard();
+                    gameOver.style.display = 'none';
+                })
+        }
     }
     var token = 0;
     const placeToken = (event) => {
@@ -93,10 +134,20 @@ const gameBoard = (() => {
             }
         }
     }
-    const tiles = document.getElementsByClassName('tile');
-    for (i = 0; i < tiles.length; i++) {
-        tiles[i].addEventListener('click', placeToken);
+    const aiGame = (event) => {
+        let playerToken = document.getElementById('token1').innerText;
+        var clickedTile = document.getElementById(event.target.id);
+        if (clickedTile.firstChild.innerText == '') {
+            clickedTile.firstChild.innerText = playerToken;
+            gameWin();
+            var delayInMS = 500;
+            setTimeout(function() {
+                computerToken()
+            }, delayInMS);
+            ;
+        }
     }
+    
     return {
         gameMode, 
         placeToken,
